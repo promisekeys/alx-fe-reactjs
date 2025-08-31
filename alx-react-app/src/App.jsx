b48fdaba5258f016630a6aa77619f577d9946e28
Ahamefula Promise <ahamefulapromise03@gmail.com>
2025-08-31 15:38:02 +0100

    Add js component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-      <WelcomeMessage />
-    </>
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
--- alx-react-app/src/main.jsx
+++ alx-react-app/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+function MainContent() {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default MainContent;
+
